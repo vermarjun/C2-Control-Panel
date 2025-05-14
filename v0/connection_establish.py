40953d23961a63ab900010c079a62196f6b8af85
vermarjun <vermarjun26@gmail.com>
2025-05-15 01:20:39 +0530

    first commit

---
+++ v0/connection_establish.py
@@ -0,0 +1,18 @@
+import os
+import asyncio
+from sliver import SliverClientConfig, SliverClient
+
+CONFIG_PATH = os.path.join('../arjun.cfg')
+
+async def main():
+    ''' Async client connect example '''
+    config = SliverClientConfig.parse_config_file(CONFIG_PATH)
+    client = SliverClient(config)
+    await client.connect()
+    operators = await client.operators()
+    sessions = await client.sessions()
+    print('Sessions: %r' % sessions)
+    print('Operators: %r' % operators)
+
+if __name__ == '__main__':
+    asyncio.run(main())
